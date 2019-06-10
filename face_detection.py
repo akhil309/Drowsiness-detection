a5b521435f67eba1e1b790b632e37785b6c41c42
akhil309 <akhilesh3091999@gmail.com>
2019-06-10 13:44:35 +0530

    face detection using haarcascading

--- face_detection.py
+++ face_detection.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 img = cv2.imread('images/img.jpeg')
 cv2.imshow('original_image',img)
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
 
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
